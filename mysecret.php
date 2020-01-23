eb1a529effbaaeb1bccf67768891cd2c6e4f661f
json-art <cotoflux@hotmail.com>
2020-01-23 15:08:39 +0100

    Testeando git

---
+++ mysecret.php
@@ -0,0 +1,11 @@
+<?php 
+
+include("secretInfo.php");
+include("vendor/autoload.php");
+
+$password = PASSWORD;  //defined in secretInfo.php
+$username = USERNAME;
+
+echo("<h1>Testing .gitignore</h1><hr>");
+echo("<h3>".$password."</h3><br>");
+echo("<h3>".$username."</h3>");
---
+++ secretInfo.php
@@ -0,0 +1,5 @@
+<?php
+
+define('PASSWORD', 'Mi secreto es ...');    
+define('USERNAME', 'me encanta el chocolate ;)') ;   
+
